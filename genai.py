35257d54452a7636ce36b64d341cb0029e8e1fa4
Tanmay Vaij <tanmayvaij22@gmail.com>
2025-07-22 14:27:33 +0530

    added system instruction
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- genai.py
+++ genai.py
@@ -10,7 +10,11 @@ gemini_client = genai.Client()
 def ask_gemini(prompt: str) -> dict:
     try:
         response = gemini_client.models.generate_content(
-            model=getenv("GEMINI_MODEL"), contents=prompt
+            model=getenv("GEMINI_MODEL"),
+            contents=prompt,
+            config={
+                "system_instruction": "You are only a random data generation which will generate some random json array on user request"
+            },
         )
 
         return loads(response.text)
